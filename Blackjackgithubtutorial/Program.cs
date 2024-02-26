e36f230446175617c00af8ac0a2a5d2a17bbab3c
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-02-26 12:11:27 +0100

    Practicing

--- Blackjackgithubtutorial/Program.cs
+++ Blackjackgithubtutorial/Program.cs
@@ -1,17 +1,20 @@
-﻿namespace Blackjackgithubtutorial
+﻿
+namespace Blackjackgithubtutorial
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Print a request for message.
+            Console.WriteLine("Give a message: ");
+
+            // Assign a new string variable. The value is the command we want to use.
+            string message = "\n" + Console.ReadLine();
+
+            // Print out the message we gave.
+            Console.WriteLine("This is it: " + "\n" + message);
         }
     }
+
 }
