2b2bd72a39e074a61e06b97c89b57481eae0ec3b
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-02-28 13:15:04 +0100

    Changes to welcome text

--- Blackjackgithubtutorial/Program.cs
+++ Blackjackgithubtutorial/Program.cs
@@ -7,13 +7,16 @@ namespace Blackjackgithubtutorial
         public static void Main(string[] args)
         {
             // Print a request for message.
-            Console.WriteLine("Give a message: ");
+            Console.WriteLine("THIS IS CASINO ROYALE");
+
+            Console.WriteLine("THIS IS BLACKJACK, WHAT CARD DO YOU WANT?: ");
 
             // Assign a new string variable. The value is the command we want to use.
             string message = "\n" + Console.ReadLine();
 
             // Print out the message we gave.
-            Console.WriteLine("This is it: " + "\n" + message);
+            Console.WriteLine("HERE IS THE CARD YOU CHOSE: " + "\n" + message);
+
         }
     }
 
